da57309621820eba795b18f2b9eb74fd8bdc81de
Yash-Pal <yashpalmnd@gmail.com>
2018-01-24 16:56:42 +0530

    404 page completed with color scheme

--- src/Components/404/NotFoundComponent.js
+++ src/Components/404/NotFoundComponent.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import HeaderLogo from './../../assets/images/yellow-logo.png';
+import BackgroundImage from './../../assets/images/banner-image.jpg';
 
 export class NotFoundComponent extends React.Component {
+    overlayMargin = '0px';
+    constructor() {
+        super();
+        this.state = {
+            overlayMargin: '0px'
+        };
+
+    }
+    componentWillMount() {
+        let Body = document.body;
+        document.title = 'Not Found';
+        Body.className = 'page-not-found';
+        Body.style.backgroundImage = 'url(' + BackgroundImage + ')';
+    }
+    componentDidMount() {
+        let overlayContent = document.getElementById('overlay-content');
+        this.setState({ 'overlayMargin': (window.innerHeight - overlayContent.offsetHeight) / 2 });
+        //this.overlayMargin = (window.innerHeight - overlayContent.offsetHeight) / 2;
+    }
     render() {
         return (
-            <div>
-                <h3>404 page not found</h3>
-                <p>We are sorry but the page you are looking for does not exist.</p>
+            <div className="wrapper">
+                <div className="body-content">
+                    <div className="container clearfix" style={{ 'padding-top': this.state.overlayMargin + 'px' }}>
+                        <div className="overlay-content" id="overlay-content" style={{ position: 'relative' }}>
+                            <Link to={'/'}>
+                                <img src={HeaderLogo} alt="logo" />
+                            </Link>
+                            <h2>Page Not Found</h2>
+                            <p>We're sorry. The page you are looking for cannot be found.</p>
+                        </div >
+                    </div>
+                </div>
             </div>
         );
     }
